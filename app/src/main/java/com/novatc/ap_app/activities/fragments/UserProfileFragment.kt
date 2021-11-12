74273ad5972f6a8093d5b3acd84ecbac1a2d42ad
Lucas Wagner <lucas.wagner@uni-oldenburg.de>
2021-11-12 12:19:31 +0100

    change manifest

--- app/src/main/java/com/novatc/ap_app/activities/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/novatc/ap_app/activities/fragments/UserProfileFragment.kt
@@ -1,11 +1,18 @@
 package com.novatc.ap_app.activities.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.novatc.ap_app.R
+import com.novatc.ap_app.activities.activities.EditProfileActivity
+import com.novatc.ap_app.activities.activities.EditWGActivity
+import com.novatc.ap_app.activities.activities.SignUpActivity
+import kotlinx.android.synthetic.main.fragment_user_profile.*
+import kotlinx.android.synthetic.main.fragment_user_profile.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +31,10 @@ class UserProfileFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        btn_edit_profile.setOnClickListener {
+            val intent = Intent (requireContext(), EditWGActivity::class.java)
+            activity?.startActivity(intent)
+        }
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,7 +46,24 @@ class UserProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val view =  inflater.inflate(R.layout.fragment_user_profile, container, false)
+        view.btn_edit_profile.setOnClickListener{
+            requireActivity().run{
+                startActivity(Intent(this, EditProfileActivity::class.java))
+                finish()
+            }
+        }
+        view.btn_edit_wg.setOnClickListener{
+            val intent = Intent (activity, EditWGActivity::class.java)
+            activity?.startActivity(intent)
+        }
+        view.btn_delet_profile.setOnClickListener{
+            val intent = Intent (activity, SignUpActivity::class.java)
+            activity?.startActivity(intent)
+            Toast.makeText(requireContext(), "Your Profile has been deleted", Toast.LENGTH_LONG).show()
+        }
+        return view
+
     }
 
     companion object {
