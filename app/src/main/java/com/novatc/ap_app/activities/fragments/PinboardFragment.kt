e11222b09e8914e0c046215fb686b298d6cd73e1
Malte Stomberg <malte.stomberg@uni-oldenburg.de>
2021-11-18 18:41:19 +0100

    translation and layout improvements

--- app/src/main/java/com/novatc/ap_app/activities/fragments/PinboardFragment.kt
+++ app/src/main/java/com/novatc/ap_app/activities/fragments/PinboardFragment.kt
@@ -35,7 +35,7 @@ class PinnboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pinnboard, container, false)
+        return inflater.inflate(R.layout.fragment_pinboard, container, false)
     }
 
     companion object {
