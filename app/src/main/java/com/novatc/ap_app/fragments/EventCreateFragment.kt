d825657fd188a699c25157f7549d5181f9005edb
Tim <tim.sigl@hotmail.de>
2021-11-19 12:31:34 +0100

    Datepicker implemented

--- app/src/main/java/com/novatc/ap_app/fragments/EventCreateFragment.kt
+++ app/src/main/java/com/novatc/ap_app/fragments/EventCreateFragment.kt
@@ -1,11 +1,15 @@
 package com.novatc.ap_app.fragments
 
 import Firestore.Fireclass
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.fragment.app.commit
 import com.novatc.ap_app.R
@@ -14,21 +18,30 @@ import kotlinx.android.synthetic.main.fragment_event_create.view.btn_safe_new_ev
 import kotlinx.android.synthetic.main.fragment_room_create.view.*
 import model.Event
 import model.Room
+import java.time.Year
+import java.util.*
 
 
-class EventCreateFragment : Fragment() {
+class EventCreateFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+    lateinit var dateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_event_create, container, false)
-        view.btn_safe_new_event.setOnClickListener {
+        val view = inflater.inflate(R.layout.fragment_event_create, container, false)
+        initDatePicker(view)
+        dateButton = view.datePickerButton
+            view.btn_safe_new_event.setOnClickListener {
             val eventList = EventFragment()
             val event = Event(
-                view.et_name_of_event.text.toString(),
-                view.et_date_of_event.text.toString(),
+                view.eventName.text.toString(),
                 Fireclass().getCurrentUserID()
             )
             Fireclass().addEventToDD(event)
@@ -38,6 +51,31 @@ class EventCreateFragment : Fragment() {
             }
         }
 
+
         return view
+    }
+
+    fun initDatePicker(view: View) {
+        val cal = Calendar.getInstance()
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+        val month = cal.get(Calendar.MONTH)
+        val year = cal.get(Calendar.YEAR)
+        view.datePickerButton.setOnClickListener {
+            DatePickerDialog(context!!, this, year, month, day).show()
+        }
+
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        Log.d("Datepicker", "Jahr: $year Monat: $month Tag: $dayOfMonth")
+        var monthString = month.toString()
+        var dayOfMonthString = dayOfMonth.toString()
+        if(month < 10)  {
+            monthString = "0$month"
+        }
+        if (dayOfMonth < 10) {
+            dayOfMonthString = "0$dayOfMonth"
+        }
+        dateButton.text = "$dayOfMonthString-$monthString-$year"
     }
 }
