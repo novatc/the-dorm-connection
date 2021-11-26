48f0e7650ce99b5a01881a0219afa9f536046e88
Tim <tim.sigl@hotmail.de>
2021-11-26 05:41:23 +0100

    removed import

--- app/src/main/java/com/novatc/ap_app/adapter/EventsAdapter.kt
+++ app/src/main/java/com/novatc/ap_app/adapter/EventsAdapter.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.novatc.ap_app.R
-import com.novatc.ap_app.model.Event
 import com.novatc.ap_app.model.EventWithUser
 
 class EventsAdapter(private val eventListItems: ArrayList<EventWithUser>): RecyclerView.Adapter<EventsAdapter.EventsViewHolder>() {
